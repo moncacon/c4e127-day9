cff66175756accb41a21334c92fc21452009a808
moncacon <thangnguyen28091996@gmail.com>
2022-03-18 18:43:25 +0700

    nine

---
+++ app.js
@@ -0,0 +1,55 @@
+//function checkAge(age) {
+//    if (age > 18) {
+//      return true;
+//    } else {
+//     return confirm('Did parents allow you?');
+//    }
+//  }
+/* toán tử dấu chấm hỏi-hk1 */
+//  let age = prompt('age');
+//  let accessAllowed = (age > 18) ? true : false ;
+//  console.log(accessAllowed);
+  /* sử dụng or ||*/
+/*let age = prompt('age');
+if( age > 18 || age < 18){
+  console.log(true);
+}*/
+/*function ask(question, yes, no) {
+  if (confirm(question)) yes()
+  else no();
+}
+
+ask(
+  "Do you agree?",
+  function() { alert("You agreed."); },
+  function() { alert("You canceled the execution."); }
+)*/
+// thay hàm mũi tên hk3
+/*  var ask = (question, yes, no)=> {
+  if (confirm(question)) yes()
+  else no();
+}
+ask(
+  "Do you agree?",
+  () => {alert("You agreed."); },
+  () => {alert("you canceled the execution."); }
+)*/
+// hk2
+/*const Number = (prompt('nhập số a'));
+const value = (prompt('nhập số b'));
+
+//let min = newarray[0];
+ //for (let i=0; i > value.split(",").length; i++) {
+ //    if(min > Number(newarray[i])){
+ //        min = newarray[i];
+ //    }
+ //}
+ //console.log(min)
+console.log(Math.min);*/
+//hk4
+ /*function myfunction() {
+  let person = prompt("please enter");
+}
+if(person = admin){
+  let password = 
+}*/
